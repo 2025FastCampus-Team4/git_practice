720b273326adfae4365256e267fc342ef87b43b3
“joy007fun” <ityeseul@gmail.com>
2025-06-27 16:49:07 +0900

    feat: Print fizzbuzz if the numbers are Divisable by 3,5,15

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for i in (1,16+1):
+    if i % 15 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else: 
+        return 'Error'
